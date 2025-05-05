f9c0e5267ce5b1b1793b6d01cd5a40a885a8e8d8
Paweł Stępień <pstep@MacBook-Air.local>
2025-05-05 21:56:20 +0200

    added search page and back button

---
+++ app/src/main/java/com/example/chatty_be/SearchUserActivity.java
@@ -0,0 +1,31 @@
+package com.example.chatty_be;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SearchUserActivity extends AppCompatActivity {
+
+
+    ImageButton backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_search_user);
+
+        backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
+
+
+
+    }
+}
