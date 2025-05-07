151a7ca414f75edf5882abbf8420b6334c033f5f
Maria-Lavinia Otelea <mlo@frankly.dk>
2025-05-07 11:45:42 +0200

    Created search functionality

--- app/src/main/java/com/example/chatty_be/SearchUserActivity.java
+++ app/src/main/java/com/example/chatty_be/SearchUserActivity.java
@@ -3,6 +3,7 @@ package com.example.chatty_be;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -10,22 +11,84 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatty_be.adapter.SearchUserRecyclerAdapter;
+import com.example.chatty_be.model.UserModel;
+import com.example.chatty_be.utils.FirebaseUtil;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class SearchUserActivity extends AppCompatActivity {
-
-
+    EditText searchInput;
+    ImageButton searchButton;
     ImageButton backButton;
+    RecyclerView recyclerView;
+
+    SearchUserRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_search_user);
 
         backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
 
+        searchInput = findViewById(R.id.search_username_input);
+        searchButton = findViewById(R.id.search_user_btn);
+        recyclerView = findViewById(R.id.search_user_recycler_view);
+
+        searchInput.requestFocus();
+
+        searchButton.setOnClickListener(v->{
+            String searchTerm = searchInput.getText().toString();
+            if(searchTerm.isEmpty() || searchTerm.length() < 2){
+                searchInput.setError("Invalid username");
+                return;
+            }
+
+            setupSearchRecyclerView(searchTerm);
+        });
+
+    }
+
+    void setupSearchRecyclerView(String searchTerm){
+
+        Query query = FirebaseUtil.allUsersCollectionReference()
+                .whereGreaterThanOrEqualTo("username", searchTerm);
+
+        FirestoreRecyclerOptions<UserModel>  options = new FirestoreRecyclerOptions.Builder<UserModel>()
+                .setQuery(query, UserModel.class).build();
+
+        adapter = new SearchUserRecyclerAdapter(options, getApplicationContext());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(adapter != null)
+            adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(adapter != null)
+            adapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(adapter != null)
+            adapter.startListening();
     }
 }
