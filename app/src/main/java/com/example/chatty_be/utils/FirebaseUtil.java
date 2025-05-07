2928d17f8e5b14e903551f317e43e8424145f318
Maria-Lavinia Otelea <mlo@frankly.dk>
2025-05-07 14:16:46 +0200

    Send chats functionality

--- app/src/main/java/com/example/chatty_be/utils/FirebaseUtil.java
+++ app/src/main/java/com/example/chatty_be/utils/FirebaseUtil.java
@@ -24,4 +24,19 @@ public class FirebaseUtil {
         return  FirebaseFirestore.getInstance().collection("users");
     }
 
+    public  static  DocumentReference getChatRoomReference(String chatRoomId){
+        return  FirebaseFirestore.getInstance().collection("chatrooms").document(chatRoomId);
+    }
+
+    public  static CollectionReference getChatRoomMessageReference(String chatroomId){
+        return  getChatRoomReference(chatroomId).collection("chats");
+    }
+
+    public  static String getChatRoomId(String userId1,String userId2 ){
+        if(userId1.hashCode()<userId2.hashCode()){
+            return userId1 +"_" + userId2;
+        }else{
+            return userId2 +"_" + userId1;
+        }
+    }
 }
